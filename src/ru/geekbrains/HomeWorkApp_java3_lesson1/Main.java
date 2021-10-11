aa3be769a253a778d8f7b38a65ce767f135af729
Mercesletifer47 <art-matv98@yandex.ru>
2021-10-11 19:29:01 +0300

    Initial commit

---
+++ src/ru/geekbrains/HomeWorkApp_java3_lesson1/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.HomeWorkApp_java3_lesson1;
+
+import ru.geekbrains.HomeWorkApp_java3_lesson1.fruits.Apple;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+ 
